adc1f2477b15deb124075dae3888248e3078e166
Tim <tim@timwheeler.com>
2018-08-30 18:40:29 -0400

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello Coders</h1>
+    <p>This is my freeCodeCamp Hackathon Ticket</p>
+    <p>Now excuse me while I go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
