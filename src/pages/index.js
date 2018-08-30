8871820a91bf833c6038fa3b5757746d4b8e9e26
Tim <tim@timwheeler.com>
2018-08-30 18:54:55 -0400

    added link to timwheeler.com

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ const IndexPage = () => (
     <h1>Hello Coders</h1>
     <p>This is my freeCodeCamp Hackathon Ticket</p>
     <p>Now excuse me while I go build something great.</p>
+
+    <a href="https://timwheeler.com" target="_blank">Check me out here</a>
+
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
